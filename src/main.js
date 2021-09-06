f3157ee4b2d4cde103ac29d3d5785ce56e00bebb
Александр <westerovs@mail.ru>
2021-09-06 11:30:16 +0300

    added pixi template

--- src/main.js
+++ src/main.js
@@ -1,3 +1,44 @@
 /* eslint-disable */
+import * as PIXI from 'pixi.js'
 
-console.log(555)
+const wrapper = document.querySelector('.wrapper')
+
+console.log(wrapper)
+
+const app = new PIXI.Application({
+  width: 400,
+  height: 400,
+  transparent: false,
+  antialias: true
+});
+
+app.renderer.backgroundColor = '0x24495D'
+
+// The application will create a canvas element for you that you
+wrapper.append(app.view);
+
+
+// load the texture we need
+app.loader.add('bunny', 'bunny.png').load((loader, resources) => {
+  // This creates a texture from a 'bunny.png' image
+  const bunny = new PIXI.Sprite(resources.bunny.texture);
+
+  // Setup the position of the bunny
+  bunny.x = app.renderer.width / 2;
+  bunny.y = app.renderer.height / 2;
+
+  // Rotate around the center
+  bunny.anchor.x = 0.5;
+  bunny.anchor.y = 0.5;
+
+  // Add the bunny to the scene we are building
+  app.stage.addChild(bunny);
+
+  // Listen for frame updates
+  app.ticker.add(() => {
+    // each frame we spin the bunny around a bit
+    bunny.rotation += 0.01;
+  });
+});
+
+console.log(app)
--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,4 @@
-const getRandomNumber = (min, max) => {
-  return Math.round(Math.random() * (max - min) + min);
-};
+const getRandomNumber = (min, max) => Math.round(Math.random() * (max - min) + min);
 
 export {
   getRandomNumber
