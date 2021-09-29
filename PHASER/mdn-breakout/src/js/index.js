37eba588ef3a9c556104b700d29ae5c3309d20ca
Александр <westerovs@mail.ru>
2021-09-29 23:16:11 +0300

    added immovable for platform

--- PHASER/mdn-breakout/src/js/index.js
+++ PHASER/mdn-breakout/src/js/index.js
@@ -37,22 +37,20 @@ function create() {
   // [2] нам необходимо добавить мяч в физическую систему,
   // т.к объект, отвечающий за физику в Phaser, не включён по умолчанию.
   game.physics.enable(ball, Phaser.Physics.ARCADE);
-  game.physics.enable(paddle, Phaser.Physics.ARCADE);
   
   // [3] установить скорость мяча через velocity (вместо ball.x += 0.3; в update)
-  ball.body.velocity.set(260, 360);
-  ball.body.gravity.x = -125
+  ball.body.velocity.set(90, 140);
+  // ball.body.gravity.x = -125
   
   ball.body.collideWorldBounds = true; // вкл столкновения
   ball.body.bounce.set(1); // вкл отскакиваемость
   
-  paddle.body.collideWorldBounds = true; // вкл столкновения
-  paddle.body.bounce.set(1); // вкл отскакиваемость
+  game.physics.enable(paddle, Phaser.Physics.ARCADE);
+  paddle.body.immovable = true; // что бы платформа не утопала
 }
 
 // код внутри update - это requestAnimations - он всё время запущен
 function update() {
   console.log('update')
-  // ball.x += 0.3;
-  // ball.y += 0.1;
+  game.physics.arcade.collide(ball, paddle); // включить обработку столкновений с мячом
 }
