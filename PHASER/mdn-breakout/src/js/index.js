0158dd8383eef57043fa5bf15ad0745657c37bb1
Александр <westerovs@mail.ru>
2021-09-29 22:00:18 +0300

    mdn-game draw ball

--- PHASER/mdn-breakout/src/js/index.js
+++ PHASER/mdn-breakout/src/js/index.js
@@ -1,4 +1,14 @@
-const game = new Phaser.Game(480, 320, Phaser.AUTO, null, {preload: preload, create: create, update: update});
+const game = new Phaser.Game(
+  480,
+  320,
+  Phaser.AUTO,
+  null, {
+      preload: preload,
+      create : create,
+      update : update
+  });
+
+let ball = null;
 
 function preload() {
     game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
@@ -7,7 +17,14 @@ function preload() {
     
     // поменять фон канваса
     game.stage.backgroundColor = '#444000';
+    //                 имя
+    game.load.image('ball', './src/img/ball.png');
 }
 
-function create() {}
+function create() {
+  // чтобы вывести наш мяч на экран, мы используем  метод add.sprite()
+  // последний параметр — это имя ↓ картинки
+  ball = game.add.sprite(50, 50, 'ball');
+}
+
 function update() {}
