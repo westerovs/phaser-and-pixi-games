f3449d5189303bc179e649ee667875586a438329
Александр <westerovs@mail.ru>
2021-09-29 21:47:57 +0300

    mdn-game create preload func

--- PHASER/mdn-breakout/src/js/index.js
+++ PHASER/mdn-breakout/src/js/index.js
@@ -1,5 +1,13 @@
 const game = new Phaser.Game(480, 320, Phaser.AUTO, null, {preload: preload, create: create, update: update});
 
-function preload() {}
+function preload() {
+    game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+    game.scale.pageAlignHorizontally = true;
+    game.scale.pageAlignVertically = true;
+    
+    // поменять фон канваса
+    game.stage.backgroundColor = '#444000';
+}
+
 function create() {}
 function update() {}
