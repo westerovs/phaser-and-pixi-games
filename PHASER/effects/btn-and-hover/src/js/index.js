00bd9419fcc9a5952ad133cbd3ea2ecb34ce7849
Александр <westerovs@mail.ru>
2021-10-04 22:47:43 +0300

    effect button complete

--- PHASER/effects/btn-and-hover/src/js/index.js
+++ PHASER/effects/btn-and-hover/src/js/index.js
@@ -1,25 +1,13 @@
 class Game {
   constructor() {
     this.game = null;
-  
-    // кнопка старт
-    this.playing = false; // пригодится сделать ракетку неподвижной
     this.startButton = null;
+    
+    this.count = 0;
   }
   
   preload = () => {
-    this.game.stage.backgroundColor = '#444000';     // поменять фон канваса
-  
-    this.game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
-    this.game.scale.pageAlignHorizontally = true;
-    this.game.scale.pageAlignVertically = true;
-
-    this.game.load.spritesheet('button', '/src/img/button.png', 120, 40);
-    this.game.load.image('ball', './src/img/ball.png');
-    this.game.load.image('paddle', './src/img/paddle.png');
-    this.game.load.image('brick', './src/img/brick.png');
-    //  В spritesheet два доп параметра определяют ширину и высоту каждого отдельного кадра в данном файле
-    this.game.load.spritesheet('ball', './src/img/wobble.png', 20, 20); // типо атлас мячиков
+    this.game.load.spritesheet('test-button', '/src/img/button.png', 120, 40);
   }
   
   create = () => {
@@ -33,29 +21,31 @@ class Game {
   init() {
     this.game = new Phaser.Game(
       480,
-      320,
+      400,
       Phaser.CANVAS,
-      null, {
+      null,
+      {
         preload: this.preload,
         create : this.create,
         update : this.update
       })
   }
-  
-  startGame() {
-    console.log('start game')
-  }
-  
+
   createStartBtn() {
     this.startButton = this.game.add.button(
-      this.game.world.width  * 0.5, // pos x
-      this.game.world.height * 0.5,// pos y
-      'button',       // имя
-      this.startGame, // Ф-ция обратного вызова, которая будет выполняться при нажатии кнопки.
-      this,           // Ссылка на this определение контекста выполнения
-      1, 0, 2         // Кадры анимации
+      this.game.world.width  * 0.5,
+      this.game.world.height * 0.5,
+      'test-button',     // имя как в preload спрайте
+      this.clickHandler, // колбек при нажатии кнопки.
+      this,              // Ссылка на this определение контекста выполнения
+      1, 0, 2            // Кадры анимации
     );
+    console.log(this.startButton)
     this.startButton.anchor.set(0.5);
+  }
+  
+  clickHandler() {
+    console.log('count', this.count += 5)
   }
 }
 
