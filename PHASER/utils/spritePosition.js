c86b3f8d58f1436ce278867fb896672a81f0b46a
Александр <westerovs@mail.ru>
2021-10-22 00:23:16 +0300

    sprite position v2

--- PHASER/utils/spritePosition.js
+++ PHASER/utils/spritePosition.js
@@ -1,15 +1,45 @@
-export default class SpritePosition {
-  constructor(sprite, scalePanel) {
-    this.sprite = sprite
+class SpritePosition {
+  constructor(sprites, scalePanel) {
+    this.sprites = sprites
+    this.checkedIndexSprite = 0
+    this.sprite = null
+    this.spriteName = null
+
     this.className = 'sprite-position-panel'
     this.scalePanel = scalePanel
+
+    this.startPositionX = null
+    this.startPositionY = null
+
+    this.elementName = null
     this.elementX = null
     this.elementY = null
     this.step = 1
     this.stepAccelerator = 10
 
+    this.init()
+  }
+
+  init = () => {
+    this.update()
+
     this.createInfoPanel()
     this.setSpritePosition()
+  }
+
+  update = () => {
+    this.sprite = Object.entries(this.sprites)[this.checkedIndexSprite][1]
+    this.spriteName = Object.entries(this.sprites)[this.checkedIndexSprite][0]
+    this.startPositionX = this.sprite.position.x
+    this.startPositionY = this.sprite.position.y
+  }
+
+  checkoutSprite = () => {
+    this.update()
+
+    this.elementName.innerHTML = `Sprite: ${this.spriteName}`
+    this.elementX.innerHTML = `X: ${Math.trunc(this.sprite.position.x)}`
+    this.elementY.innerHTML = `Y: ${Math.trunc(this.sprite.position.y)}`
   }
 
   createInfoPanel = () => {
@@ -19,7 +49,7 @@ export default class SpritePosition {
         top: 0;
         left: 0;
         min-width: 80px;
-        padding: 10px;
+        padding: 10px 20px;
         font-size: ${this.scalePanel ? this.scalePanel : 3}em;
         background: rgba(0, 0, 0, 0.7);
         color: white;
@@ -27,36 +57,48 @@ export default class SpritePosition {
         z-index: 999;
     `)
 
+    this.elementName = document.createElement('p')
     this.elementX = document.createElement('p')
     this.elementY = document.createElement('p')
 
+    wrapInfo.append(this.elementName)
     wrapInfo.append(this.elementX)
     wrapInfo.append(this.elementY)
     document.body.append(wrapInfo)
 
+    this.elementName.innerHTML = `Sprite: ${this.spriteName}`
     this.elementX.innerHTML = `X: ${Math.trunc(this.sprite.position.x)}`
     this.elementY.innerHTML = `Y: ${Math.trunc(this.sprite.position.y)}`
   }
 
   setSpritePosition() {
-    let startPositionX = this.sprite.position.x
-    let startPositionY = this.sprite.position.y
-
     document.addEventListener('keydown', (key) => {
-      if (key.code === 'ArrowDown') startPositionY += this.step
-      if (key.code === 'ArrowUp') startPositionY -= this.step
-      if (key.code === 'ArrowLeft') startPositionX -= this.step
-      if (key.code === 'ArrowRight') startPositionX += this.step
+      if (key.code === 'ArrowDown') this.startPositionY += this.step
+      if (key.code === 'ArrowUp') this.startPositionY -= this.step
+      if (key.code === 'ArrowLeft') this.startPositionX -= this.step
+      if (key.code === 'ArrowRight') this.startPositionX += this.step
+
       // важен порядок → сперва зажимаем shift, потом кнопку
-      if (key.code === 'ArrowDown' && key.shiftKey === true) startPositionY += this.stepAccelerator
-      if (key.code === 'ArrowUp' && key.shiftKey === true) startPositionY -= this.stepAccelerator
-      if (key.code === 'ArrowLeft' && key.shiftKey === true) startPositionX -= this.stepAccelerator
-      if (key.code === 'ArrowRight' && key.shiftKey === true) startPositionX += this.stepAccelerator
+      if (key.code === 'ArrowDown' && key.shiftKey === true) this.startPositionY += this.stepAccelerator
+      if (key.code === 'ArrowUp' && key.shiftKey === true) this.startPositionY -= this.stepAccelerator
+      if (key.code === 'ArrowLeft' && key.shiftKey === true) this.startPositionX -= this.stepAccelerator
+      if (key.code === 'ArrowRight' && key.shiftKey === true) this.startPositionX += this.stepAccelerator
 
-      this.elementX.innerHTML = `X: ${Math.trunc(startPositionX)}`
-      this.elementY.innerHTML = `Y: ${Math.trunc(startPositionY)}`
+      this.elementName.innerHTML = `Sprite: ${this.spriteName}`
+      this.elementX.innerHTML = `X: ${Math.trunc(this.startPositionX)}`
+      this.elementY.innerHTML = `Y: ${Math.trunc(this.startPositionY)}`
 
-      this.sprite.position.set(startPositionX, startPositionY)
+      this.sprite.position.set(this.startPositionX, this.startPositionY)
+
+      // сменить sprite
+      if (key.code === 'ArrowLeft' && key.ctrlKey === true) {
+        this.checkedIndexSprite--
+        this.checkoutSprite()
+      }
+      if (key.code === 'ArrowRight' && key.ctrlKey === true) {
+        this.checkedIndexSprite++
+        this.checkoutSprite()
+      }
     })
   }
 }
