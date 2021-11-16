801f84f4b112e2238d621d30cc2f357e39d6f858
Александр <westerovs@mail.ru>
2021-11-16 03:48:38 +0300

    create buttons

--- PHASER/games/status-game-panel/src/js/index.js
+++ PHASER/games/status-game-panel/src/js/index.js
@@ -32,11 +32,15 @@ class Game {
     this.game.load.image('panel', './src/img/panel.png')
     this.game.load.image('light', './src/img/light.png')
     this.game.load.image('life', './src/img/life.png')
+    this.game.load.spritesheet('btnLife', './src/img/btn-life.png', 120, 40)
+    this.game.load.spritesheet('btnLight', './src/img/btn-light.png', 120, 40)
   }
   
   create = () => {
     this.createPanel(0, 50, 'life')
     this.createPanel(100, 150, 'light')
+    this.createBtn(100, 300, 'btnLife', this.onHandlerLifeBtn)
+    this.createBtn(300, 300, 'btnLight', this.onHandlerLightBtn)
   }
   
   update = () => {
@@ -77,6 +81,26 @@ class Game {
       newLife.anchor.set(0.5)
       this.panel.add(newLife)
     }
+  }
+  
+  onHandlerLifeBtn = () => {
+    console.log('life')
+  }
+  
+  onHandlerLightBtn = () => {
+    console.log('light')
+  }
+  
+  createBtn(x, y, sprite, callback) {
+    this.startButton = this.game.add.button(
+      x, // pos x
+      y,// pos y
+      sprite,  // имя
+      callback, // Ф-ция обратного вызова, которая будет выполняться при нажатии кнопки.
+      this,      // Ссылка на this определение контекста выполнения
+      1, 0, 2    // кадры анимации
+    );
+    this.startButton.anchor.set(0.5);
   }
 }
 
