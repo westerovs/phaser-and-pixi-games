49d50673885610311d4b91b830fb70be9a63677b
Александр <westerovs@mail.ru>
2022-03-10 13:47:28 +0300

    crystal game update

---
+++ #games/puzzle experiment game/src/js/Crystal.js
@@ -0,0 +1,92 @@
+export default class Part {
+  constructor(game, x, y, anchor, sprite, disabled = false, initAngle) {
+    this.game = game
+    this.positionPartX = x
+    this.positionPartY = y
+    this.anchor = anchor
+    this.anchorX = this.anchor[0]
+    this.anchorY = this.anchor[1]
+    this.sprite = sprite
+    this.disabled = disabled
+    this.initAngle = initAngle
+    
+    this.block = null
+    this.progress = 0
+    this.startProgress = this.initAngle
+    this.val = 0
+    this.nex = null
+    this.degreeAngle = null
+    this.startTouches = null
+    
+    this.init()
+  }
+  
+  init = () => {
+    this.#create()
+  }
+  
+  #create = () => {
+    this.block = this.game.add.sprite(this.positionPartX + (this.anchorX * 100), this.positionPartY + (this.anchorY * 100), this.sprite)
+    
+    this.block.inputEnabled = this.disabled ? false : true
+    this.block.angle = this.startProgress
+    this.block.anchor.set(...this.anchor)
+    
+    this.block.events.onInputDown.add(this.#touchStart)
+    this.block.events.onInputUp.add(this.#touchUp)
+    
+    this.game.world.add(this.block)
+    return this.block
+  }
+  
+  #touchStart = (sprite, pointer) => {
+    this.block.isPressed = true
+    
+    this.startProgress = this.progress
+    this.nex = parseFloat(this.block.angle)
+    
+    // получаем первые координаты касания
+    this.startTouches = {
+      x: pointer.x,
+      y: pointer.y
+    }
+    
+    this.game.input.addMoveCallback(this.#touchMove)
+  }
+  
+  #touchMove = (pointer) => {
+    if (!this.block.isPressed) return
+    if (!pointer.isDown || !this.startTouches) return
+    
+    // получаем координаты текущего касания
+    const touch = {
+      x: pointer.x,
+      y: pointer.y
+    }
+    
+    // центр объекта
+    const center = {
+      x: this.block.centerX + (this.anchorX * 100),
+      y: this.block.centerY + (this.anchorY * 100)
+    }
+    
+    // вычисление угла
+    let angleDistance = Math.atan2(
+      (center.x - touch.x) * (center.y - this.startTouches.y)
+      - (center.y - touch.y) * (center.x - this.startTouches.x),
+      
+      (center.x - touch.x) * (center.x - this.startTouches.x)
+      + (center.y - touch.y) * (center.y - this.startTouches.y),
+    )
+    
+    angleDistance *= -1
+    this.degreeAngle = angleDistance * (180 / Math.PI)
+    
+    this.val = this.degreeAngle + this.nex
+    this.block.angle = this.val
+  }
+  
+  #touchUp = () => {
+    this.block.isPressed = false
+  }
+}
---
+++ #games/puzzle experiment game/src/js/const.js
@@ -0,0 +1,56 @@
+const crystalName = {
+  crystalBody : 'crystalBody',
+  crystalLeft : 'crystalLeft',
+  crystalTop  : 'crystalTop',
+  crystalRight: 'crystalRight',
+}
+
+const crystalPartsParams = [
+  {
+    key   : crystalName.crystalBody,
+    isDisabled: true,
+    x     : 330,
+    y     : 300,
+    anchor: [0.5, 0.5],
+    initAngle : 0,
+  },
+  {
+    key   : crystalName.crystalLeft,
+    isDisabled: false,
+    x     : 200,
+    y     : 240,
+    anchor: [0, 1],
+    // initAngle: -60,
+    initAngle: 0,
+  },
+  {
+    key   : crystalName.crystalTop,
+    isDisabled: false,
+    x     : 282,
+    y     : 138,
+    anchor: [1, 0.599],
+    // initAngle: 80,
+    initAngle: 0,
+  },
+  {
+    key   : crystalName.crystalRight,
+    isDisabled: false,
+    x     : 460,
+    y     : 125,
+    anchor: [1, 0.65],
+    // initAngle: 120,
+    initAngle: 0,
+  },
+]
+
+const crystalParts = {
+  crystalBody : null,
+  crystalLeft : null,
+  crystalTop  : null,
+  crystalRight: null,
+}
+
+export {
+  crystalPartsParams,
+  crystalParts
+}
