967a4082c0ea691c0a86aef2aad3610e4bff7e95
Александр <westerovs@mail.ru>
2022-03-14 01:36:20 +0300

    puzzle without physics: top right complete

--- #games/puzzle experiment game - без физики/src/js/components/Crystal.js
+++ #games/puzzle experiment game - без физики/src/js/components/Crystal.js
@@ -1,5 +1,21 @@
 import {crystals} from '../const.js'
 
+const autoRotateOnError = (game, crystal, cb) => {
+  if (cb) game.input.deleteMoveCallback(cb)
+  
+  const rotation = +crystal.rotation.toFixed(2)
+  
+  let angleError = null
+  if (rotation > 0) angleError = crystal.initAngle
+  else angleError = angleError = crystal.angle - crystal.initAngle
+  
+  return game.add.tween(crystal)
+    .to(
+      {angle: angleError},
+      Phaser.Timer.HALF / 10, Phaser.Easing.Linear.None, true
+    )
+}
+
 class IsWin {
   constructor(game) {
     this.game = game
@@ -7,14 +23,15 @@ class IsWin {
     
     this.rotation = null
     this.booleanRotate = true
+    this.test = null
   }
   
   checkOnFinishRotate = (crystal) => {
-    console.log(this.rotation)
     this.crystal = crystal
     this.rotation = +crystal.rotation.toFixed(2)
     
     crystals.find(item => {
+      // если crystalTop активен
       if (item.key === 'crystalTop' && !item.isComplete) {
         switch (this.crystal.key) {
           case 'crystalLeft':
@@ -22,31 +39,54 @@ class IsWin {
           case 'crystalTop':
             break;
           case 'crystalRight':
-            if (this.rotation >= 0.90 && this.rotation <= 1.12) {
+            if (this.rotation >= 0.80 && this.rotation <= 0.99) {
               console.warn('СТОЛКНОВЕНИЕ')
               this.booleanRotate = false
+              autoRotateOnError(this.game, crystal, this.rotation)
             }
             if (this.rotation >= -2.15 && this.rotation <= -2) {
               console.warn('СТОЛКНОВЕНИЕ')
               this.booleanRotate = false
+              autoRotateOnError(this.game, crystal, this.rotation)
             }
+            break;
+        }
+      }
+      if (item.key === 'crystalTop' && item.isComplete) {
+        switch (this.crystal.key) {
+          case 'crystalLeft':
+            break;
+          case 'crystalTop':
+            break;
+          case 'crystalRight':
+            // if (this.rotation >= 0.80 && this.rotation <= 0.99) {
+            //   console.warn('СТОЛКНОВЕНИЕ')
+            //   this.booleanRotate = false
+            //   autoRotateOnError(this.game, crystal, this.rotation)
+            // }
+            // if (this.rotation >= -2.15 && this.rotation <= -2) {
+            //   console.warn('СТОЛКНОВЕНИЕ')
+            //   this.booleanRotate = false
+            //   autoRotateOnError(this.game, crystal, this.rotation)
+            // }
             break;
         }
       }
     })
     
-    this.autoRotate()
+    this.autoRotateOnFinish()
     
     return this.booleanRotate
   }
   
   // авто-доводка если фигура становится на базу
-  autoRotate = () => {
+  autoRotateOnFinish = () => {
     if (Math.abs(this.rotation) >= 0 && Math.abs(this.rotation) <= 0.15) {
       this.crystal.tint = 0x2DE200
       this.crystal.disabled = true
-
-      game.add.tween(this.crystal)
+      this.crystal.isComplete = true
+      
+      this.game.add.tween(this.crystal)
         .to({
           angle: 0,
         }, Phaser.Timer.HALF / 2, Phaser.Easing.Linear.None, true)
@@ -76,25 +116,25 @@ export default class Part {
     
     //
     this.mainGroup = game.add.group()
-    this.groupChildren = game.add.group()
     this.block = null
-    this.childBlock = null
     
     // параметры вращения
     this.progress = 0
     this.startProgress = this.initAngle
-    this.val = 0
+    this.finishVal = 0
     this.nex = null
     this.degreeAngle = null
     this.startTouches = null
     
+    // components
     this.isWin = null
+    
     this.init()
   }
   
   init = () => {
-    this.#createBlock()
     this.isWin = new IsWin(this.game)
+    this.#createBlock()
   }
   
   #createBlock = () => {
@@ -103,13 +143,14 @@ export default class Part {
     this.block.initAngle  = this.initAngle
     
     this.block.inputEnabled = this.disabled ? false : true
+    
     this.block.angle = this.startProgress
     this.block.anchor.set(...this.anchor)
   
     this.mainGroup.add(this.block)
     this.game.world.add(this.mainGroup)
     this.#initEvents()
-  
+    
     return this.block
   }
   
@@ -162,20 +203,31 @@ export default class Part {
     angleDistance *= -1
 
     this.degreeAngle = angleDistance * (180 / Math.PI)
-    this.val = this.degreeAngle + this.nex
+    this.finishVal = this.degreeAngle + this.nex
   
+    this.#checkRotate()
+    
+    this.block.angle = this.finishVal
+  }
+  
+  #checkRotate = () => {
+    // если срабатывает событие ошибки, или финиша
     if (!this.isWin.checkOnFinishRotate(this.block)) {
-      console.log(`!!!!!!!!!`)
-      return;
+      console.log(111)
+      this.block.angle = this.initAngle
+      // this.block.inputEnabled = false
+      this.isWin.booleanRotate = true
+      this.game.input.deleteMoveCallback(this.#onTouchMove)
+      // return;
     }
-    this.block.angle = this.val
   }
   
   #OnTouchUp = () => {
     this.block.isPressed = false
-    
     if (this.block.disabled) return
     
-    this.block.angle = this.initAngle
+    // this.block.angle = this.initAngle
+    autoRotateOnError(this.game, this.block)
+      .onComplete.add(() => this.game.input.addMoveCallback(this.#onTouchMove))
   }
 }
